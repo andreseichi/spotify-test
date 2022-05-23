f380e70606d29dcf1c6fc980be11ad9ccc92d315
andreseichi <andre_seichi@hotmail.com>
2022-05-23 19:21:43 -0300

    passando as variaveis de ambiente pras api routes por segurança

--- src/pages/api/token.ts
+++ src/pages/api/token.ts
@@ -1,7 +1,22 @@
 import { token } from '../../services/token';
 
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+
 export default async function getToken() {
-  const { data } = await token.post('token', 'grant_type=client_credentials');
+  const config = {
+    headers: {
+      Authorization:
+        'Basic ' +
+        Buffer.from(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64'),
+    },
+  };
+
+  const { data } = await token.post(
+    'token',
+    'grant_type=client_credentials',
+    config
+  );
   const { access_token } = data;
 
   return access_token;
--- src/services/token.ts
+++ src/services/token.ts
@@ -1,14 +1,8 @@
 import axios from 'axios';
 
-const CLIENT_ID = process.env.CLIENT_ID;
-const CLIENT_SECRET = process.env.CLIENT_SECRET;
-
 export const token = axios.create({
   baseURL: 'https://accounts.spotify.com/api/',
   headers: {
     'Content-Type': 'application/x-www-form-urlencoded',
-    Authorization:
-      'Basic ' +
-      Buffer.from(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64'),
   },
 });
